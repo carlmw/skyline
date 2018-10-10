035e309d803a6b5301a1d743deaaacdf90e24623
Carl Whittaker <carl@geckoboard.com>
2018-10-10 22:02:41 +0100

    Render the connected table of cities

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { PureComponent } from 'react';
+import TableDataSource from './TableDataSource';
+import Table from './Table';
 
 class App extends PureComponent {
+  fetchData() {
+    return fetch('/cities.json').then(response => response.json());
+  }
+
+  renderTable(props) {
+    return <Table {...props} />
+  }
+
   render() {
-    return <div>Hello world</div>;
+    return (
+      <TableDataSource onInit={this.fetchData}>
+        {this.renderTable}
+      </TableDataSource>
+    );
   }
 }
 
